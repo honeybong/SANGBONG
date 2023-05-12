4e9d1a5f7fe975f153faf96faa97da20c7eb0053
PARK SANG BONG <parksangbong01@github.cmo>
2023-05-12 15:39:28 +0900

    aaaaa
    sssssssss

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ const App = (props) => {
   console.log('3')
   console.log('4')
   console.log('5')
+  console.log('6')
+  console.log('7')
   return (
     <div className='test'>
       <header className='test-header'>
